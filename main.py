e1b29418197092a7262b6046ce94e259fde9356f
boss11503 <Poletaev16pol@gmail.com>
2025-05-25 13:54:44 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from pygame import *
 
-class GameSprite(sprite.Sprite):
+class GameSpite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65,65))
+        self.image = transform.scale(image.load(player_image), (65, 65))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+    
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player(GameSprite):
+class Player(GameSpite):
     def update(self):
         keys = key.get_pressed()
         if keys[K_LEFT] and self.rect.x > 5:
@@ -23,7 +24,7 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
-class Enemy(GameSprite):
+class Enemy(GameSpite):
     direction = "left"
     def update(self):
         if self.rect.x <= 470:
@@ -36,8 +37,8 @@ class Enemy(GameSprite):
             self.rect.x += self.speed
 
 class Wall(sprite.Sprite):
-    def __init__(self,color_1, color_2, color_3, wall_x, wall_y, wall_width, wall_height):
-        super().__init__()
+    def __init__(self, color_1, color_2, color_3, wall_x, wall_y, wall_width, wall_height):
+        super(). __init__()
         self.color_1 = color_1
         self.color_2 = color_2
         self.color_3 = color_3
@@ -48,42 +49,43 @@ class Wall(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = wall_x
         self.rect.y = wall_y
+
     def draw_wall(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-            
-win_width = 1000
-win_height = 700
 
+win_width = 900
+win_height = 800
 window = display.set_mode((win_width, win_height))
-display.set_caption('игра')
+display.set_caption("Лабиринт")
 background = transform.scale(image.load("background.jpg"), (win_width, win_height))
+
 player = Player('hero.png', 5, win_height - 80, 4)
-monster = Enemy('cyborg.png', win_width - 80, 280, 2)
-final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
+monster1 = Enemy('cyborg.png', win_width - 20, 280, 5)
+monster2 = Enemy('cyborg.png', win_width - 150, 300, 5)
+monster3 = Enemy('cyborg.png', win_width - 600, 320, 5)
+final = GameSpite('treasure.png', win_width - 100, 50, 0)
 
-w1 = Wall(154, 205, 50, 100, 20, 800, 15)
-w2 = Wall(154, 205, 50, 100, 600, 750, 20)
-w3 = Wall(154, 205, 50, 150, 500, 250, 15)
-w4 = Wall(154, 205, 50, 150, 20, 10, 500)
-w5 = Wall(154, 207, 50, 550, 20, 10, 300)
-w6 = Wall(154, 203, 50, 400, 20, 10, 380)
-
-
-
-
-
-
+w1 = Wall(154, 205, 50, 100, 20, 675, 20)
+w2 = Wall(154, 205, 50, 100, 750, 650, 20)
+w3 = Wall(154, 205, 50, 750, 120, 20, 650)
+w4 = Wall(154, 205, 50, 100, 20, 20, 650)
+w5 = Wall(154, 205, 50, 200, 120, 20, 550)
+w6 = Wall(154, 205, 50, 200, 120, 450, 20)
+w7 = Wall(154, 205, 50, 630, 20, 20, 100)
+w8 = Wall(154, 205, 50, 200, 650, 150, 20)
+w9 = Wall(154, 205, 50, 450, 650, 200, 20)
+w10 = Wall(154, 205, 50, 450, 155, 20, 520)
 
 
 game = True
 finish = False
 clock = time.Clock()
-FPS = 60 
+FPS = 60
 
 font.init()
-font = font.Font(None, 70)
-win = font.render('ты не лох', True, (255, 215, 0))
-lose = font.render('ты лох', True, (180, 0, 0))
+font = font.Font(None, 150)
+win = font.render('ты победил!', True, (255, 215, 0))
+lose = font.render('ты проиграл:(', True, (180, 0, 0))
 
 mixer.init()
 mixer.music.load('jungles.ogg')
@@ -92,19 +94,23 @@ mixer.music.play()
 money = mixer.Sound('money.ogg')
 kick = mixer.Sound('kick.ogg')
 
-while  game:
-    for e in  event.get():
+while game:
+    for e in event.get():
         if e.type == QUIT:
             game = False
 
     if finish != True:
         window.blit(background, (0, 0))
         player.update()
-        player.update()
+        monster1.update()
+        monster2.update()
+        monster3.update()
 
-        window.blit(background, (0, 0))
         player.reset()
-        monster.reset()
+        monster1.reset()
+        monster2.reset()
+        monster3.reset()
+        final.reset()
 
         w1.draw_wall()
         w2.draw_wall()
@@ -112,18 +118,18 @@ while  game:
         w4.draw_wall()
         w5.draw_wall()
         w6.draw_wall()
+        w7.draw_wall()
+        w8.draw_wall()
+        w9.draw_wall()
+        w10.draw_wall()
 
-
-
-        if sprite.collide_rect(player, monster) or sprite.collide_rect(player, w1) or sprite.collide_rect(player, w2) or sprite.collide_rect(player, w3):
+        if sprite.collide_rect(player, monster1) or sprite.collide_rect(player, monster2) or sprite.collide_rect(player, monster3) or sprite.collide_rect(player, w1) or sprite.collide_rect(player, w2) or sprite.collide_rect(player, w3) or sprite.collide_rect(player, w4) or sprite.collide_rect(player, w5) or sprite.collide_rect(player, w6) or sprite.collide_rect(player, w7) or sprite.collide_rect(player, w8) or sprite.collide_rect(player, w9):
             finish = True
-            window.blit(lose, (200, 200))
+            window.blit(lose, (100, 350))
             kick.play()
-        
         if sprite.collide_rect(player, final):
             finish = True
-            window.blit(win, (200, 200))
+            window.blit(win, (100, 350))
             money.play()
-
     display.update()
     clock.tick(FPS)
